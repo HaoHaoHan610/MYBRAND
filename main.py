d99079bf7c5ab53f4f667c87326628f9e19e7774
HaoHaoHan610 <kocoemail610@gmail.com>
2026-01-04 09:22:58 +0700

    ADD ALL FILES

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from src.workflows import WorkFlow
 
 run = WorkFlow()
 pa = PotentialAnalysis(
-    major="IT", gpa=3.2, year=2,
-    strengths=["Python", "SQL"],
+    major="IT", gpa=3.68, year=2,
+    strengths=["Python", "SQL","DSA"],
     language={"English": "B2"},
     achievements=["Built a small web app"],
-    mentor=None
+    mentor=True
 )
 print(run._analyze_advise(pa))
