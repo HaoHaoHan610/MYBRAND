4e6b029a6f2a08744330040c80333a045a6909ec
HaoHaoHan610 <kocoemail610@gmail.com>
2026-01-12 17:43:04 +0700

    ADD ALL FILES

--- src/FLASKAPI/app.py
+++ src/FLASKAPI/app.py
@@ -4,22 +4,19 @@ from api.controllers.input import bp as bp_input
 from api.controllers.output import bp as bp_output
 def create_app():
     app = Flask(__name__)
-
+    
     # Enable CORS for all routes
     CORS(app, resources={r"/*": {"origins": "*"}})
-
+    
     app.register_blueprint(bp_input)
     app.register_blueprint(bp_output)
 
-    @app.route("/")
+
+    @app.route('/')
     def home():
-        return "<h1>Khanh bu cu</h1>"
-
+        return '<h1>Khanh bu cu</h1>'
     return app
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug= True,host='0.0.0.0',port=5000)
 
-
-# Create the WSGI app at module import time so gunicorn finds `app`
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
